e2fddfbd03d185dbaa4541bf7e8d8ba5b62f8652
oleh.lozovyi <ol-hb@conciliolabs.com>
2024-07-11 14:57:53 +0300

    FIX for extended equipment for bersion 1.0.5

--- ThePlanetCrafterEquipmentExtend/ThePlanetCrafterEquipmentExtend/Class1.cs
+++ ThePlanetCrafterEquipmentExtend/ThePlanetCrafterEquipmentExtend/Class1.cs
@@ -1,16 +1,11 @@
 ﻿using BepInEx;
-using HarmonyLib;
-using HarmonyLib.Tools;
-using MijuTools;
 using SpaceCraft;
-using Steamworks;
 using System;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using Object = UnityEngine.Object;
+using Unity.Netcode;
 
 namespace ThePlanetCrafterEquipmentExtend
 {
@@ -19,74 +14,75 @@ namespace ThePlanetCrafterEquipmentExtend
     {
         public const string pluginGuid = "crazymetal.theplanetcrafter.EquipmentExtend";
         public const string pluginName = "EquipmentExtend";
-        public const string pluginVersion = "0.0.1";
+        public const string pluginVersion = "0.0.2";
 
         public void Awake()
         {
             Logger.LogInfo(pluginName + " " + pluginVersion + " " + "starting...");
-            Harmony harmony = new Harmony(pluginGuid);
+            var harmony = new Harmony(pluginGuid);
         
             MethodInfo originalHasSameEquipableTypeItemInEquipment = AccessTools.Method(typeof(PlayerEquipment), "HasSameEquipableTypeItemInEquipment");
             MethodInfo patchHasSameEquipableTypeItemInEquipment = AccessTools.Method(typeof(EquipmentExtender), "HasSameEquipableTypeItemInEquipment_Patch");
             harmony.Patch(originalHasSameEquipableTypeItemInEquipment, new HarmonyMethod(patchHasSameEquipableTypeItemInEquipment));
 
-            MethodInfo originalTrueRefreshContent = AccessTools.Method(typeof(InventoryDisplayer), "TrueRefreshContent");
-            MethodInfo patchTrueRefreshContent = AccessTools.Method(typeof(EquipmentExtender), "TrueRefreshContent_Patch");
-            harmony.Patch(originalTrueRefreshContent,postfix:new HarmonyMethod(patchTrueRefreshContent));
-            
             MethodInfo originalUpdateGaugesDependingOnEquipment = AccessTools.Method(typeof(PlayerGaugesHandler), "UpdateGaugesDependingOnEquipment");
             MethodInfo patchUpdateGaugesDependingOnEquipment = AccessTools.Method(typeof(EquipmentExtender), "UpdateGaugesDependingOnEquipment_Patch");
-            harmony.Patch(originalUpdateGaugesDependingOnEquipment, postfix:new HarmonyMethod(patchUpdateGaugesDependingOnEquipment));
+            harmony.Patch(originalUpdateGaugesDependingOnEquipment, finalizer: new HarmonyMethod(patchUpdateGaugesDependingOnEquipment));
 
+            MethodInfo originalTrueRefreshContent = AccessTools.Method(typeof(InventoryDisplayer), "TrueRefreshContent");
+            MethodInfo patchTrueRefreshContent = AccessTools.Method(typeof(EquipmentExtender), "TrueRefreshContent_Patch");
+            harmony.Patch(originalTrueRefreshContent,finalizer:new HarmonyMethod(patchTrueRefreshContent));
 
         }
-        public static void TrueRefreshContent_Patch(InventoryDisplayer __instance, UnityEngine.UI.GridLayoutGroup ___grid, Inventory ___inventory, ref Vector2 ___originalSizeDelta)
+        
+        public static void TrueRefreshContent_Patch(InventoryDisplayer __instance, int ____selectionIndex, UnityEngine.UI.GridLayoutGroup ____grid, GroupSelector ___groupSelector,
+            Inventory ____inventoryInteracting, Inventory ____inventory, ref Vector2 ____originalSizeDelta, LogisticManager ____logisticManager)
         {
-            if (__instance == null)
+          
+
+            if (____originalSizeDelta == Vector2.zero)
             {
-                return;
+                ____originalSizeDelta = __instance.GetComponent<RectTransform>().sizeDelta;
             }
 
-            if (___originalSizeDelta == Vector2.zero)
-            {
-                ___originalSizeDelta = __instance.GetComponent<RectTransform>().sizeDelta;
-            }
-
-            if (___grid.cellSize != Vector2.zero && ___originalSizeDelta != Vector2.zero)
+            if (____grid.cellSize != Vector2.zero && ____originalSizeDelta != Vector2.zero)
             {           
 
-                if (___inventory.GetSize() <= 35) {__instance.GetComponent<RectTransform>().sizeDelta = new Vector2(___originalSizeDelta.x, ___originalSizeDelta.y); 
+                if (____inventory.GetSize() <= 35) {__instance.GetComponent<RectTransform>().sizeDelta = new Vector2(____originalSizeDelta.x, ____originalSizeDelta.y); 
                 } else {
-                    __instance.GetComponent<RectTransform>().sizeDelta = new Vector2(___originalSizeDelta.x+275f, ___originalSizeDelta.y+100f);
+                    __instance.GetComponent<RectTransform>().sizeDelta = new Vector2(____originalSizeDelta.x+275f, ____originalSizeDelta.y+100f);
                 }
                
-                if (___inventory.GetSize() > 35)
+                if (____inventory.GetSize() > 35)
                 {
                     float containerWidth = __instance.GetComponent<RectTransform>().sizeDelta.x;
                     float containerheidht = __instance.GetComponent<RectTransform>().sizeDelta.y;
                  //   Console.WriteLine("containerWidth:" + containerWidth + ", containerheidht" + containerheidht);
-                    for (int i = 100; i > 35; i--)
+                    for (int i = 100; i > 10; i--)
                     {
                         int rows = (int)Math.Floor(containerWidth / (float)i);
                         int columns = (int)Math.Floor(containerheidht / (float)i);
-                  //      Console.WriteLine("rows:" + containerWidth + ", columns" + containerheidht);
+                     //Console.WriteLine("rows:" + containerWidth + ", columns" + containerheidht);
                         int size = rows * columns;
-                 //      Console.WriteLine("Size:" + size + ", inventorySize" + ___inventory.GetSize());
-                        if ((rows * columns) >= ___inventory.GetSize())
+                     //Console.WriteLine("Size:" + size + ", inventorySize" + ___inventory.GetSize());
+                        if ((rows * columns) >= ____inventory.GetSize())
                         {
-                            ___grid.cellSize = new Vector2(i, i);
-                  //          Console.WriteLine("CellSize:" + i);
+                            ____grid.cellSize = new Vector2(i, i);
+                            ____grid.spacing = new Vector2(0.5f, 0.5f);
+                     //Console.WriteLine("CellSize:" + i);
                             break;
                         }
                     }
                 }
                 __instance.SetIconsPositionRelativeToGrid();
+                ____selectionIndex = -1;
             }
 
         }
+       
         public static bool HasSameEquipableTypeItemInEquipment_Patch(GroupItem groupItemClicked, PlayerEquipment __instance)
         {
-            using (List<WorldObject>.Enumerator enumerator = __instance.GetInventory().GetInsideWorldObjects().GetEnumerator())
+            using (List<WorldObject>.Enumerator enumerator = (List<WorldObject>.Enumerator)__instance.GetInventory().GetInsideWorldObjects().GetEnumerator()) 
             {
                 while (enumerator.MoveNext())
                 {
@@ -98,24 +94,28 @@ namespace ThePlanetCrafterEquipmentExtend
             }
             return false;
         }
-
-        public static void UpdateGaugesDependingOnEquipment_Patch(Inventory equipmentInventory ,PlayerGaugeOxygen ___gOxygen) {
-            {
-                if (___gOxygen == null) return;
-                float increase = 0;
-                ___gOxygen.AddToInitialValue(increase);
-                foreach (WorldObject insideWorldObject in equipmentInventory.GetInsideWorldObjects())
+       
+        public static void UpdateGaugesDependingOnEquipment_Patch(PlayerGaugesHandler __instance,Inventory equipmentInventory,float ____initialValue , NetworkVariable<float> ____oxygenValue, NetworkVariable<float> ____oxygenMaxValue)
+        {
+            
+             float oxygen = ____initialValue;
+             foreach (WorldObject insideWorldObject in equipmentInventory.GetInsideWorldObjects())
                 {
                     GroupItem groupItem = (GroupItem)insideWorldObject.GetGroup();
 
                     if (groupItem.GetEquipableType() == DataConfig.EquipableType.OxygenTank)
                     {
-                        increase += groupItem.GetGroupValue();
+                        oxygen += groupItem.GetGroupValue();
                         
                     }
                 }
-                ___gOxygen.AddToInitialValue(increase);
-            }
+                if (____oxygenMaxValue.Value > oxygen)
+                {
+                    ____oxygenValue.Value = oxygen;
+                }
+
+                ____oxygenMaxValue.Value = oxygen;
+            
         }
     }
 }
